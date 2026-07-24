6d5785fbdc91ecaf60a03b257d2e642a32df79f0
JoJoRafa <joaquim.rafael.cunha@gmail.com>
2026-07-24 02:12:48 -0300

    web_test.commit

--- site_basico.py
+++ site_basico.py
@@ -5,6 +5,7 @@ st.title  ('Cadastro do cliente')
 nome = st.text_input ('Digite o nome do cliente')
 endereço = st.text_input ('Digite o endereço')
 dt_nas = st.date_input('Escolha a data de Nascimento')
+password = st.text_input("Digite sua senha forte", type="password")
 tipo_cliente = st.selectbox('Tipo do cliente', 
                             ["Pessoa física", "Pessoa jurídica"])
 cadastrar = st.button('Cadastrar cliente')
