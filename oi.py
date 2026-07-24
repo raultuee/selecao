ea6d486134d9bdc108718fff107a93861290b1a0
JoJoRafa <joaquim.rafael.cunha@gmail.com>
2026-07-24 01:55:02 -0300

    site_test.commit

--- oi.py
+++
@@ -1,1 +0,0 @@
-print('olá raul gay')
---
+++ pagina.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title  ('Cadastro do cliente')
+
+nome = st.text_input ('Digite o nome do cliente')
+endereço = st.text_input ('Digite o endereço')
+dt_nas = st.date_input('Escolha a data de Nascimento')
+tipo_cliente = st.selectbox('Tipo do cliente', 
+                            ["Pessoa física", "Pessoa jurídica"])
+cadastrar = st.button('Cadastrar cliente')
+
+if cadastrar:
+    with open ('Cliente.csv','a', encoding='utf8') as arquivo:
+        arquivo.write(f'{nome},{endereço},{dt_nas},{tipo_cliente}')
+        st.success('Cliente cadastrado com sucesso.')
+    
+
+
+
+
