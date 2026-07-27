25630b5ea7f314567469e9aac81129406d839499
JoJoRafa <joaquim.rafael.cunha@gmail.com>
2026-07-27 01:41:55 -0300

    testsenha.commit

--- pagina.py
+++ pagina.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit import streamlit 
 
 st.title  ('Cadastro do cliente')
 
@@ -6,16 +7,20 @@ nome = st.text_input ('Digite o nome do cliente')
 endereço = st.text_input ('Digite o endereço')
 dt_nas = st.date_input('Escolha a data de Nascimento')
 password = st.text_input("Digite sua senha forte", type="password")
+senha = st.text_input("Digite sua senha: ")
+
 tipo_cliente = st.selectbox('Tipo do cliente', 
                             ["Pessoa física", "Pessoa jurídica"])
 cadastrar = st.button('Cadastrar cliente')
 
 if cadastrar:
     with open ('Cliente.csv','a', encoding='utf8') as arquivo:
-        arquivo.write(f'{nome},{endereço},{dt_nas},{tipo_cliente}')
+        arquivo.write(f'{nome},{endereço},{dt_nas},{senha},{tipo_cliente}')
         st.success('Cadastro do viadin concedido.')
     
+if len(senha) < 4:
+    print("Precisa de uma senha forte")
 
 
-
-
+# lembrando que precisa estar no arquivo
+# para rodar: streamlit run pagina.py
